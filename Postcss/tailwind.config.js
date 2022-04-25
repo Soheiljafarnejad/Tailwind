3948e921f47ad2304769efbf7c5bdae26a31c01a
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-04-25 18:42:50 +0430

    disclosure

--- Postcss/tailwind.config.js
+++ Postcss/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '128': "32rem",
+      },
+    },
   },
   plugins: [],
-}
+};
